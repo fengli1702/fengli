c31fb065fe430a592b9a645778686dcb3d32db8b
feng <119935962+fengli1702@users.noreply.github.com>
2024-04-14 12:47:00 +0800

    Delete code_c directory

--- code_c/C_single/excercise/0的字符串.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-int main(){
-    char *str="1230000000";
-    printf("::%s",str);
-    for(int i=0;i<strlen(str);i++){
-        printf("%c ",str[i]);
-    }
-}
--- code_c/C_single/excercise/hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-    char name[10];
-    printf("input:");
-    scanf("%s",name);
-    printf("hello %s",name);
-    return 0;
-}
